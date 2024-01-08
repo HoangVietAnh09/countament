b8ffeae7c044597dcd07e5de90aad003332d2317
VietAnh <vietanh092004@gmail.com>
2024-01-08 08:45:49 +0700

    Countament

---
+++ countament.js
@@ -0,0 +1,18 @@
+let cnt = document.getElementById("countLabel").textContent;
+cnt = Number(cnt);
+document.getElementById("decreaseBtn").onclick = function(){
+    cnt += 1;
+    document.getElementById("countLabel").textContent = cnt;
+}
+document.getElementById("resetBtn").onclick = function(){
+    cnt = 0;
+    document.getElementById("countLabel").textContent = cnt;
+}
+document.getElementById("increaseBtn").onclick = function(){
+    if(cnt !== 0){
+        cnt -= 1;
+        document.getElementById("countLabel").textContent = cnt;
+    }
+   
+}
+
